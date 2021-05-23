4e8f5a5e4b66ce2a047bb587e938901ab89d6abd
Perla Ricardez <pfricardez@gmail.com>
2021-05-22 21:53:00 -0500

    Creacion de servicio de autorizacion y seguridad

--- user.oauth2.0/src/main/java/com/springbank/user/oauth20/OAuth2Application.java
+++ user.oauth2.0/src/main/java/com/springbank/user/oauth20/OAuth2Application.java
@@ -2,14 +2,14 @@ package com.springbank.user.oauth20;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config;.
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-public class Application {
+public class OAuth2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(OAuth2Application.class, args);
 	}
 
 }
